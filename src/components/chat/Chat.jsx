04ad345d0c66ec1099a64fdd2e285c55a089b1ef
ramrodrigo <rrodrigo2004@yahoo.com>
2024-10-09 22:34:29 +0800

    add auto scroll using useRef hook

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './chat.css';
 import EmojiPicker from 'emoji-picker-react';
 
 export default function Chat() {
 	const [open, setOpen] = useState(false);
 	const [text, setText] = useState('');
+	const endRef = useRef();
+
+	useEffect(() => {
+		endRef.current?.scrollIntoView({ behavior: 'smooth' });
+	}, []);
 
 	const handleEmoji = (e) => {
 		setText((prev) => prev + e.emoji);
@@ -57,6 +62,7 @@ export default function Chat() {
 						<span>1 min ago</span>
 					</div>
 				</div>
+				<div ref={endRef} />
 			</div>
 			<div className='bottom'>
 				<div className='icons'>
